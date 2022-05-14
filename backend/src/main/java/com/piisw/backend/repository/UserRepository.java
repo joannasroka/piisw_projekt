5098b0b5fc49583542921019d882d7435cffa5f2
joannasroka <joannasroka98@gmail.com>
2022-05-14 07:26:59 +0200

    Add query to find user by ID and by email ignoring case

--- backend/src/main/java/com/piisw/backend/repository/UserRepository.java
+++ backend/src/main/java/com/piisw/backend/repository/UserRepository.java
@@ -1,8 +1,27 @@
 package com.piisw.backend.repository;
 
+import com.piisw.backend.entity.account_activation.AccountStatus;
 import com.piisw.backend.entity.user.User;
+import com.piisw.backend.exception.DatabaseEntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAndAccountStatus(String email, AccountStatus accountStatus);
+
     boolean existsByEmailIgnoreCase(String email);
+
+    default User getById(Long id) {
+        return findById(id).orElseThrow(DatabaseEntityNotFoundException::new);
+    }
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    default User getByEmailIgnoreCase(String email) {
+        return findByEmailIgnoreCase(email).orElseThrow(DatabaseEntityNotFoundException::new);
+    }
+
+    List<User> findAllByAccountStatus(AccountStatus accountStatus);
 }
