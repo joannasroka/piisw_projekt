8e32c3ff9abbe735e6f64679a05bb63f1d288fb5
Michał Kierzkowski <kierzkowski89@gmail.com>
2022-06-17 17:23:52 +0200

    Test fixes

--- frontend/ebilet/src/app/tickets/resolvers/buy-ticket-resolver/buy-ticket.resolver.spec.ts
+++ frontend/ebilet/src/app/tickets/resolvers/buy-ticket-resolver/buy-ticket.resolver.spec.ts
@@ -1,5 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { BuyTicketResolver } from './buy-ticket.resolver';
 
@@ -9,7 +11,9 @@ describe('BuyTicketResolver', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        RouterTestingModule,
+        SharedModule
       ]
     });
     resolver = TestBed.inject(BuyTicketResolver);
